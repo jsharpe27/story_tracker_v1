831763dc2d21458a978507093bcae7431fa0617e
Jordan Sharpe <86000863+jsharpe27@users.noreply.github.com>
2023-09-24 22:43:00 -0400

    working on editing individual stories

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -41,6 +41,11 @@ const Card = () => {
       alert('save button clicked')
  }
 
+ function handleEditClick(storyId){
+
+    setEditing((prevEditing => !prevEditing))
+ }
+
   const storyCardElements = storyData.map(function(story){
     return (
       <div key={story.id} className='p-2 border border-black m-[.5rem]'>
@@ -48,7 +53,7 @@ const Card = () => {
           { editing ? <input type='number' value={editWordCount} placeholder='Update word count' onChange={(e) => setEditWordCount(e.target.value)} />  : <p>wordCount: {story.wordCount}</p> }
           { editing ? <input type='radio' value={editIsSubmitted} onChange={(e) => setEditIsSubmitted(e.target.value)} /> : <p>isSubmitted: {story.isSubmitted}</p> }
           { editing ? <input type='text' value={editDescription} placeholder='Update description' onChange={(e) => setEditDescription} /> : <p>description: {story.description}</p> }
-          <button onClick={() => setEditing((prevEditing => !prevEditing))}>{ !editing ? 'Edit' : 'Discard changes'}</button>
+          <button onClick={() => handleEditClick(story.id)}>{ !editing ? 'Edit' : 'Discard changes'}</button>
           { editing ? <button onClick={handleSaveClick}>Save</button> : null}
           <button onClick={() => deleteStory(story.id)}>Delete Story</button> 
       </div> 
