9ab1cfe8334018018af615bcd86a550a95ed8dd6
Jordan Sharpe <86000863+jsharpe27@users.noreply.github.com>
2023-10-28 12:08:21 -0400

    update main nifo

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,2 +1,2 @@
 export const infoListItems = ['A dashboard geared to writers:', 
-'-Track your stories', '-Visit helpful resources', '-prompt generator (coming soon!)']
+'-Track your stories', '-Brainstorm and save notes', '-links to helpful resources']
